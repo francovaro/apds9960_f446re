4050cd58a8202964e99f287a8e8d1981cf6639c9
Francesco Varani <francovaro@gmail.com>
2020-11-13 17:11:52 +0100

    added lin_uart

--- src/main.c
+++ src/main.c
@@ -8,12 +8,21 @@
   ******************************************************************************
 */
 
+#include "lib_uart.h"
 
 #include "stm32f4xx.h"
+#include "string.h"
+
 			
 
 int main(void)
 {
+	UART_lib_config(e_UART_2, DISABLE, 0, 0);
 
-	for(;;);
+	UART_lib_sendData("START\n", strlen("START\n"));
+
+	while(1)
+	{
+
+	}
 }
