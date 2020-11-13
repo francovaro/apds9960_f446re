3c457bca7468da4712d5b5bab7b60539406698d2
Francesco Varani <francovaro@gmail.com>
2020-11-13 17:29:25 +0100

    added lib systick

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 */
 
 #include "lib_uart.h"
+#include "lib_systick.h"
 
 #include "stm32f4xx.h"
 #include "string.h"
@@ -18,6 +19,8 @@
 int main(void)
 {
 	UART_lib_config(e_UART_2, DISABLE, 0, 0);
+
+	libsystick_set_systick(1000);
 
 	UART_lib_sendData("START\n", strlen("START\n"));
 
